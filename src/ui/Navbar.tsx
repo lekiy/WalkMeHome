fb16782c0d85b74b61104cef2cd723d4557673af
Kevin Annen <lekiyminin@gmail.com>
2023-08-16 23:51:12 -0500

    Working towards adding match route.

--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
 import LoginForm from "./LoginForm";
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -8,6 +9,29 @@ function Navbar() {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Walk Me Home
         </Typography>
+        <Button
+          variant="text"
+          color="inherit"
+          sx={{ ":active": { background: "white" } }}
+        >
+          Your Match
+        </Button>
+        <NavLink
+          to="/"
+          style={({ isActive }) => {
+            return {
+              backgroundColor: isActive ? "white" : "transparent",
+            };
+          }}
+        >
+          <Button
+            variant="text"
+            color="inherit"
+            sx={{ ":active": { background: "white" } }}
+          >
+            Search Dogs
+          </Button>
+        </NavLink>
         <LoginForm />
       </Toolbar>
     </AppBar>
